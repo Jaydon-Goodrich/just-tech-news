bef8da7c61ca04ef01ea8fe69220a06aff64e4a8
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-10-28 18:37:15 -0600

    comment and upvote

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -5,24 +5,28 @@ router.get('/', (req, res) => {
     Comment.findAll({
 
     })
-    .then(dataRes => res.json(dataRes))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+        .then(dataRes => res.json(dataRes))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    // check the session
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // use the id from the session
+            user_id: req.session.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 router.delete('/:id', (req, res) => {
@@ -31,11 +35,11 @@ router.delete('/:id', (req, res) => {
             id: req.params.id
         }
     })
-    .then(resData => res.json(resData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+        .then(resData => res.json(resData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 module.exports = router;
